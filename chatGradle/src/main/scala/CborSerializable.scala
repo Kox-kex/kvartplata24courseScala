4c076270c63ea56a1f2679e55ff2f5754c5d2896
Kox-kex <64608308+Kox-kex@users.noreply.github.com>
2020-12-02 22:45:37 +0400

    chat

--- chatGradle/src/main/scala/CborSerializable.scala
+++
@@ -1,6 +0,0 @@
-//Marker trait to tell Akka to serialize messages into CBOR using Jackson
-// for sending over the network See application.conf where it is bound to a serializer.
-
-trait CborSerializable {
-
-}
